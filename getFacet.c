16182ab76175d76e7768707f93b9b60071f149ad
Vatsal Sanjay <vatsalsanjay@gmail.com>
2024-07-27 20:47:09 +0200

    refactor: Update includes and variable declarations in getFacet.c
    cleaned up due to a conflict

--- getFacet.c
+++ getFacet.c
@@ -3,35 +3,21 @@
 # vatsalsanjay@gmail.com
 # Physics of Fluids
 */
-#include "axi.h"
-#include "navier-stokes/centered.h"
-#define FILTERED
-#include "src-local/contact-fixed.h"
-#include "two-phase.h"
-#include "navier-stokes/conserving.h"
-#include "tension.h"
-#include "reduced.h"
 
-double thetaA = 60.0*pi/180.0, thetaR = 40.0*pi/180.0;
+#include "utils.h"
+#include "output.h"
+#include "fractions.h"
 
-// scalar f[];
-vector h[];
+
+scalar f[];
 char filename[80];
 
-double contact_angle_tanh(double u, double thetaA, double thetaB, double width) {
-    return thetaA + 0.5 * (thetaB - thetaA) * (1 + tanh(u / width));
-}
 
 int main(int a, char const *arguments[])
 {
   sprintf (filename, "%s", arguments[1]);
   restore (file = filename);
-  #if TREE
-    f.prolongation = fraction_refine;
-  #endif
-  f.height = h;
-  h.t[left] = contact_angle(contact_angle_tanh(u.y[], thetaA, thetaR, 0.1));
-  boundary((scalar *){f});
+
   FILE * fp = ferr;
   output_facets(f,fp);
   fflush (fp);
